39e3807be2bd624376daf96416f079e5072d3c7b
Ram Mehta <ram@fivestars.com>
2014-02-11 18:17:41 -0500

    More fixes. Make more robust.

--- pptwitter/poll.py
+++ pptwitter/poll.py
@@ -2,12 +2,10 @@ from __future__ import division
 
 import ConfigParser
 import logging
-import numpy as np
-import os
-import random
 import signal
 import sys
 import time
+import tweepy
 
 from multiprocessing import Pool
 from tweepy import Cursor
@@ -26,19 +24,6 @@ ranker = PercentilePoetryRanker(
     config.get("Ranker", "percentiles"))
 
 
-def rank(tweet):
-    """ Rank each tweet.
-
-    This function must be pickleable. This acheived by making this a
-    module level function.
-    """
-
-    pid = os.getpid()
-    np.random.seed(pid)
-    random.seed(pid)
-    return ranker.rank([tweet.text])
-
-
 class TweetPoller(object):
 
     pool = Pool(processes=int(config.get("Poller", "processes")))
@@ -52,13 +37,13 @@ class TweetPoller(object):
     @property
     def since_id(self):
         try:
-            return config.get("Twitter", "since_id")
+            return int(config.get("Poller", "since_id"))
         except ConfigParser.NoOptionError:
             return None
 
     @since_id.setter
     def since_id(self, id):
-        config.set("Twitter", "since_id", id)
+        config.set("Poller", "since_id", id)
         with open(config_path, "wb") as configfile:
             config.write(configfile)
 
@@ -76,11 +61,15 @@ class TweetPoller(object):
             if self.since_id is not None:
                 kwargs["since_id"] = self.since_id
 
-            for page in Cursor(api.search, **kwargs).pages():
-                nextpoll = time.time() + self.poll_interval
-                logger.info("Processing tweets...")
-                self.process_page(page)
-                self.sleep_until(nextpoll)
+            try:
+                for page in Cursor(api.search, **kwargs).pages():
+                    nextpoll = time.time() + self.poll_interval
+                    logger.info("Processing tweets...")
+                    self.process_page(page)
+                    self.sleep_until(nextpoll)
+            except tweepy.error.TweepError:
+                logger.error("Unknown error.", exc_info=True)
+
             self.sleep_until(nextpoll)
 
     def process_page(self, page):
@@ -89,6 +78,13 @@ class TweetPoller(object):
         scores = ranker.rank(list(t.text for t in tweets))
         for tweet, score in zip(tweets, scores):
             # Put score into redis and create a link to some server to show scores.
-            avgscore = sum(score.values()) / len(score)
-            logger.info('%s...: %.1f%%' % (tweet.text[:20], avgscore))
-            api.update_status("Your poem scored %.1f%%" % avgscore, tweet.id)
+            self.update_status(tweet, score)
+
+    def update_status(tweet, score):
+        avgscore = sum(score.values()) / len(score)
+        screen_name = tweet.user.screen_name
+        logger.info('%s @%s %s...: %.1f%%' % (tweet.id, screen_name, tweet.text[:20], avgscore))
+        try:
+            api.update_status("@%s Your poem scored %.1f%%" % (screen_name, avgscore), tweet.id)
+        except tweepy.error.TweepError:
+            logger.error("Error updating status.", exc_info=True)
